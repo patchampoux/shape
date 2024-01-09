5a1e60169fcbe6d1f3cce817cf8969e2aba4d5cc
Patrick Champoux <champoux.patrick@gmail.com>
2024-01-09 14:12:08 -0500

    Translate.

--- web/app/themes/shape/inc/theme.php
+++ web/app/themes/shape/inc/theme.php
@@ -79,6 +79,27 @@ function shape_setup() {
 add_action( 'after_setup_theme', 'shape_setup' );
 
 /**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function shape_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'partoutavecpitou' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'partoutavecpitou' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+
+add_action( 'widgets_init', 'shape_widgets_init' );
+
+/**
  * Clean up head
  */
 remove_action( 'wp_head', 'rsd_link' ); // remove really simple discovery link
